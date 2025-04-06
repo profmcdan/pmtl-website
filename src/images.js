4b500c043c547d1ba5fa79d59631a4a2d392c6cd
Bashir <amodubashir@yahoo.com>
2025-04-06 22:49:34 +0100

    duplicate

--- src/images.js
+++ src/images.js
@@ -1,26 +1,26 @@
-import allprods from "./assets/images/allprods.webp"
-import pp110 from "./assets/images/pp110.webp"
-import pb300 from "./assets/images/pb300.webp"
-import duplexer from "./assets/images/duplexer.webp"
-import pmtl7630 from "./assets/images/pmtl7630.webp"
-import pp211 from "./assets/images/pp211.webp"
-import pr400 from "./assets/images/pr400.webp"
-import pronto from "./assets/images/pronto.webp"
-import aboutIso from "./assets/images/iso.jpg"
-import iso from "./assets/images/iso-logo.webp"
-import tech from "./assets/images/tech.webp"
-import team from "./assets/images/team.webp"
-import poc from "./assets/images/poc.webp"
-import pp311 from "./assets/images/pp311.webp"
-import aboutPage from "./assets/images/About-us-1.webp"
-import Gate from  "./assets/images/PMTL_Gate.webp" 
-import programCode7630 from "./assets/images/PMTL7630.zip"
-import programCode6901 from "./assets/images/PMTL6901.zip" 
-import programCodepp110 from "./assets/images/PP110.zip"
-import programCodepp211 from "./assets/images/PP211.zip"
-import programCodepp311 from "./assets/images/PP311.zip"
-import programCodepm300 from "./assets/images/PM300.zip"
-import programCodepr300 from "./assets/images/PR300.zip"
+import allprods from "./assets/images/allprods.webp";
+import pp110 from "./assets/images/pp110.webp";
+import pb300 from "./assets/images/pb300.webp";
+import duplexer from "./assets/images/duplexer.webp";
+import pmtl7630 from "./assets/images/pmtl7630.webp";
+import pp211 from "./assets/images/pp211.webp";
+import pr400 from "./assets/images/pr400.webp";
+import pronto from "./assets/images/pronto.webp";
+import aboutIso from "./assets/images/iso.jpg";
+import iso from "./assets/images/iso-logo.webp";
+import tech from "./assets/images/tech.webp";
+import team from "./assets/images/team.webp";
+import poc from "./assets/images/poc.webp";
+import pp311 from "./assets/images/pp311.webp";
+import aboutPage from "./assets/images/About-us-1.webp";
+import Gate from "./assets/images/PMTL_Gate.webp";
+import programCode7630 from "./assets/images/PMTL7630.zip";
+import programCode6901 from "./assets/images/PMTL6901.zip";
+import programCodepp110 from "./assets/images/PP110.zip";
+import programCodepp211 from "./assets/images/PP211.zip";
+import programCodepp311 from "./assets/images/PP311.zip";
+import programCodepm300 from "./assets/images/PM300.zip";
+import programCodepr300 from "./assets/images/PR300.zip";
 // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/allprods.webp",
 // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/pp110.webp",
 // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/pb300.webp",
@@ -38,18 +38,18 @@ import programCodepr300 from "./assets/images/PR300.zip"
 // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/factory.webp",
 // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/tech1.webp"
 // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PMTL_Gate.webp"
-  // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP110.zip"
-  // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP211.zip"
-  // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP311.zip"
-  // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PR300.zip"
-  // "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP311.zip"
-  
+// "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP110.zip"
+// "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP211.zip"
+// "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP311.zip"
+// "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PR300.zip"
+// "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/PP311.zip"
+
 export const webSiteImage = {
   allProducts: allprods,
   pp110: pp110,
   pb300: pb300,
-  duplexer:duplexer,
-  pmtl7630:pmtl7630, 
+  duplexer: duplexer,
+  pmtl7630: pmtl7630,
   pp211: pp211,
   pr400: pr400,
   pronto: pronto,
@@ -57,20 +57,18 @@ export const webSiteImage = {
   poc: poc,
   pp311: pp311,
   iso: iso,
-  aboutIso:aboutIso,
-  aboutPage:aboutPage,
-  pb300:pb300,
-  pr400:pr400,
-  // factory:,
+  aboutIso: aboutIso,
+  aboutPage: aboutPage,
+  pr400: pr400,
   tech: tech,
   Gate: Gate,
-  programCode7630:programCode7630,
-  programCode6901:programCode6901,
-  programCodepp110:programCodepp110,
-  programCodepp211:programCodepp211,
-  programCodepp311:programCodepp311,
-  programCodepr300:programCodepr300,
-  programCodepm300:programCodepm300,
-  banner: "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/banner.jpg",
-
+  programCode7630: programCode7630,
+  programCode6901: programCode6901,
+  programCodepp110: programCodepp110,
+  programCodepp211: programCodepp211,
+  programCodepp311: programCodepp311,
+  programCodepr300: programCodepr300,
+  programCodepm300: programCodepm300,
+  banner:
+    "https://pmtlproducts.s3.us-east-2.amazonaws.com/resources/banner.jpg",
 };
